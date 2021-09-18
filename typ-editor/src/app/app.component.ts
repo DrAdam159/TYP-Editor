04f8df42e411eb67b4d94126bfc63cc4fa11ffee
Adam <adam.dr110@gmail.com>
2021-09-18 19:04:06 +0200

    Added function for handling file on input.

--- typ-editor/src/app/app.component.ts
+++ typ-editor/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typ-editor';
+  title = 'Online TYP File editor';
 }
--- typ-editor/src/app/file-upload/file-upload.component.ts
+++ typ-editor/src/app/file-upload/file-upload.component.ts
@@ -7,12 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileUploadComponent implements OnInit {
 
-  fileName!: string;
+  fileName: string;
+  fileToUpload: File | null;
+  fileContent: string;
+  fileContent2: Uint8Array | null;
 
-  constructor() { }
+  constructor() { 
+    this.fileToUpload = null;
+    this.fileName = '';
+    this.fileContent = '';
+    this.fileContent2 = null;
+  }
 
   onFileSelected(): void{
     console.log('upload button');
+  }
+
+  handleFileInput(event: any): void {
+    this.fileToUpload = event.target.files[0];
+
+    if (this.fileToUpload) {
+      this.fileName = this.fileToUpload.name;
+      console.log(this.fileToUpload);
+
+      var reader = new FileReader();
+      reader.readAsArrayBuffer(this.fileToUpload);
+      //reader.readAsText(this.fileToUpload);
+
+      reader.onload = () => {
+        //this.fileContent = reader.result as string;
+        //console.log(this.fileContent);
+
+        var buffer = reader.result as ArrayBuffer;
+        this.fileContent2 = new Uint8Array(buffer);
+      };
+
+      reader.onerror = () => {
+        console.log(reader.error);
+      };
+    }
+    
   }
 
   ngOnInit(): void {
