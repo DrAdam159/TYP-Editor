b2b7a9b209a6eac78f4fde1db6e13f003d2c72d1
Adam <adam.dr110@gmail.com>
2021-10-16 19:43:53 +0200

    Made some layout changes

--- typ-editor/src/app/app.module.ts
+++ typ-editor/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatTreeModule} from '@angular/material/tree';
 import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { PolylineComponent } from './polyline/polyline.component';
@@ -42,7 +43,8 @@ import { EditorComponent } from './editor/editor.component';
     MatTreeModule,
     MatListModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
