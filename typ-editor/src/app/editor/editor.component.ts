cf8bcd8a232d1860ed7964f4dfa5d17d1b3a81de
Adam <adam.dr110@gmail.com>
2021-11-09 01:08:27 +0100

    Added some circle drawing calculations

--- typ-editor/src/app/editor/editor.component.ts
+++ typ-editor/src/app/editor/editor.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 import { ActivatedRoute } from '@angular/router';
 import { GraphicElement } from 'src/TYP_File_lib/TypFile_blocks/GeneralDataBlocks/GraphicElement';
 import { FileService } from '../services/file.service';
-import { fromEvent, Subscription } from 'rxjs';
+import { fromEvent, Subscription, zip } from 'rxjs';
 import { switchMap, takeUntil, pairwise } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 import { Bitmap } from 'src/TYP_File_lib/Utils/Bitmap';
@@ -161,6 +161,12 @@ export class EditorComponent implements OnInit, AfterViewInit {
     const canvasEl: HTMLCanvasElement = this.myCanvas?.nativeElement;
     this.rectangleToolCaptureEvents(canvasEl);
   }
+
+  useCircle(): void {
+    this.stopToolUse();
+    const canvasEl: HTMLCanvasElement = this.myCanvas?.nativeElement;
+    this.circleToolCaptureEvents(canvasEl);
+  }
   
   private brushToolCaptureEvents(canvasEl: HTMLCanvasElement) {
     this.mouseSub = fromEvent(canvasEl, 'mousedown')
@@ -233,7 +239,6 @@ export class EditorComponent implements OnInit, AfterViewInit {
     let rh = y - 1;
     rw = rw - rw % this.scaleNum;
     rh = rh - rh % this.scaleNum;
-    //console.log(rw + " " + rh );
     //this.itemBitmap.setPixel(rw / this.scaleNum, rh / this.scaleNum, new Color(255, 0, 0, 255));
     this.context.fillStyle = this.color;
     this.context.fillRect(rw, rh, this.scaleNum, this.scaleNum);
@@ -383,4 +388,102 @@ export class EditorComponent implements OnInit, AfterViewInit {
     this.interpolateLine({ x: currentPos.x, y: prevPos.y }, currentPos);
     this.interpolateLine({ x: prevPos.x, y: currentPos.y }, { x: currentPos.x, y: currentPos.y });
   }
+
+  private circleToolCaptureEvents(canvasEl: HTMLCanvasElement) {
+    this.mouseSub = fromEvent(canvasEl, 'mousedown')
+      .pipe(
+        switchMap(e => {
+          return fromEvent(canvasEl, 'mousemove').pipe(
+            takeUntil(fromEvent(canvasEl, 'mouseup')),
+            takeUntil(fromEvent(canvasEl, 'mouseleave')),
+            pairwise()
+          );
+        })
+      )
+      .subscribe((res) => {
+        const rect = canvasEl.getBoundingClientRect();
+        const prevMouseEvent = res[0] as MouseEvent;
+        const currMouseEvent = res[1] as MouseEvent;
+
+        if(this.lineStart == false) {
+          this.lineStartX =  prevMouseEvent.clientX - rect.left;
+          this.lineStartY = prevMouseEvent.clientY - rect.top;
+          this.lineStart = true;
+        }    
+        const currentPos = {
+          x: currMouseEvent.clientX - rect.left,
+          y: currMouseEvent.clientY - rect.top
+        };
+
+        this.drawCircle({x: this.lineStartX, y: this.lineStartY}, currentPos);
+      });
+  }
+
+  convertCoordinates(coordinates: { x: number; y: number }) {
+    let rw = coordinates.x - 1;
+    let rh = coordinates.y - 1;
+    rw = (rw - rw % this.scaleNum) / this.scaleNum ;
+    rh = (rh - rh % this.scaleNum) / this.scaleNum;
+
+    return {x: rw, y: rh};
+  }
+
+  // https://www.redblobgames.com/grids/circle-drawing/
+  // https://www.varsitytutors.com/hotmath/hotmath_help/topics/equation-of-a-circle
+  private inside_circle(center: { x: number; y: number }, tile: { x: number; y: number }, radius: number): boolean {
+    let dx = center.x - tile.x,
+        dy = center.y - tile.y;
+    let distance = Math.sqrt(dx*dx + dy*dy);
+
+    console.log(distance + " " + radius);
+    return distance <= radius;
+  }
+
+  drawCircle(prevPos: { x: number; y: number }, currentPos: { x: number; y: number }): void {
+
+    let prevCoordinates = this.convertCoordinates(prevPos);
+    let curCoordinates = this.convertCoordinates(currentPos);
+    Math.pow(4, 0.5)
+    let radius = Math.sqrt((curCoordinates.x - prevCoordinates.x) * (curCoordinates.x - prevCoordinates.x) +
+                            (curCoordinates.y - prevCoordinates.y) * (curCoordinates.y - prevCoordinates.y)
+    ) / 2;
+
+    //console.log(prevCoordinates.x + " " + curCoordinates.x + " " + radius);
+
+    for (let y = 0; y < this.itemBitmap.height / this.scaleNum; y++) {
+      for (let x = 0; x < this.itemBitmap.width / this.scaleNum; x++) {
+          if (this.inside_circle(prevCoordinates, { x: x, y: y }, radius)) {
+            console.log("true " + x + " " + y);
+            this.drawColorCell2(x, y);
+          }
+      }
+    }
+  }
+
+  private drawColorCell2(x: number, y: number): void {
+    if (!this.context) {
+      return;
+    }
+    this.context.fillStyle = this.color;
+    this.context.fillRect(x, y, this.scaleNum, this.scaleNum);
+    //this.updateBitmap();
+  }
+
+  // drawCircle(prevPos: { x: number; y: number }, currentPos: { x: number; y: number }): void {
+  //   let center = prevPos
+  //   let radius = Math.abs(prevPos.x - currentPos.x);
+  //   let top = Math.floor(center.y - radius),
+  //   bottom =  Math.ceil(center.y + radius),
+  //   left   = Math.floor(center.x - radius),
+  //   right  =  Math.ceil(center.x + radius);
+
+  //   for (let y = top; y <= bottom; y++) {
+  //       for (let x = left; x <= right; x++) {
+  //           if (this.inside_circle(center, { x: x, y: y }, radius)) {
+  //             this.drawColorCell(x, y);
+  //           }
+  //       }
+  //   }
+  // }  
+
 }
