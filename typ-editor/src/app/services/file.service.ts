e46548f34dd97c007f8f34e0c1937e3a91be3ef3
Adam <adam.dr110@gmail.com>
2022-03-04 19:57:02 +0100

    refactor: arrayBufferToBase64

--- typ-editor/src/app/services/file.service.ts
+++ typ-editor/src/app/services/file.service.ts
@@ -539,9 +539,9 @@ export class FileService {
   }
 
   arrayBufferToBase64( buffer: ArrayBuffer ) {
-    var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
+    let binary = '';
+    const bytes = new Uint8Array( buffer );
+    const len = bytes.byteLength;
     for (var i = 0; i < len; i++) {
         binary += String.fromCharCode( bytes[ i ] );
     }
